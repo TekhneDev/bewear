941a72b9c1882cfc2bc01b00bc27d8cd5e5b11ba
Carla Mariana <carlamab2808@outlook.com>
2026-01-12 22:40:02 -0300

    .

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typeNextConfig: {
+    ignoreDuringBuilds: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
